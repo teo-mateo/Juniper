d2ac7d8fd6207f76110a9d065eefd76a0a670aac
Teo <teomati@gmail.com>
2013-10-01 14:22:45 +0200

    stuffz3

--- blog/views/contraptions/loto649.py
+++ blog/views/contraptions/loto649.py
@@ -1,1 +1,53 @@
-__author__ = 'Teo'
+import json
+import csv
+
+
+
+__author__ = 'Teo hrc! Ptiu!'
+
+jsonfile = "F:/_work/gits/juniper/static/contraptions/loto649ro.json"
+csvfile = "F:/_work/gits/juniper/static/contraptions/loto649ro.csv"
+
+input = '2 17 22 36 39 42'
+
+def load_json():
+    obj = json.loads(open(jsonfile).read())
+    print obj
+    print 'done loading json'
+    return obj
+
+def parse_obj(obj, input):
+    res = [(1,0), (2,0), (3,0), (4,0), (5,0),(6,0)]
+    in_arr = str(input).split(' ')
+    for c in obj['alldraws']:
+        r = 0
+        for i in in_arr:
+            if int(i) in c['n']:
+                r += 1
+        if r > 0:
+            print str(c['n']) + str(r) + ' matches.'
+            res[r-1] = (r, res[r][1] + 1)
+
+    return res
+
+def load_csv():
+    with open(csvfile) as f:
+        reader = csv.reader(f, delimiter=',')
+        draws = list()
+
+        for row in reader:
+            draw = dict()
+            draw[u'd'] = row[0]
+            draw[u'n'] = [int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6])]
+            draws.append(draw)
+
+        d = dict()
+        d[u'alldraws'] = draws
+        return d
+
+if __name__ == '__main__':
+    obj = load_csv()
+    #obj = load_json()
+    print type(obj[u"alldraws"][0])
+    res = parse_obj(obj, input)
+    print res
--- blog/views/contraptions/quickunionUF.py
+++ blog/views/contraptions/quickunionUF.py
@@ -1,1 +1,41 @@
 __author__ = 'Teo'
+
+
+class QuickUnionUF:
+
+    def __init__(self, N):
+        self.idz = [None] * N
+        for x in range(0, N):
+            self.idz[x] = x
+
+    def root(self, i):
+        while self.idz[i] != i:
+            i = self.idz[i]
+
+        return self.idz[i]
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def union(self, p, q):
+        root_p = self.root(p)
+        root_q = self.root(q)
+
+        self.idz[root_p] = root_q
+
+
+
+
+if __name__ == "__main__":
+    c = QuickUnionUF(10)
+    c.union(6, 2)
+    c.union(5, 2)
+    c.union(5, 3)
+    c.union(9, 2)
+
+    if c.connected(9, 3):
+        print ("9 and 3 connected.")
+    else:
+        print ("9 and 3 not connected.")
+
+    print c.idz
