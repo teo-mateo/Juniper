2bc2cf54a6344b23505219acd3ebbc8159b97971
teo-mateo <teomati@gmail.com>
2013-11-06 16:17:33 +0200

    chrome extension

--- contraptions/simbya/popup.js
+++ contraptions/simbya/popup.js
@@ -4,7 +4,7 @@
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     var req = new XMLHttpRequest();
-    req.open("POST", "http://127.0.0.1:8000/section/links/addlink/", true);
+    req.open("POST", "http://127.0.0.1:8000/section/links/addlink/", false);
     req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     req.send("csrfmiddlewaretoken=cI1YkCpPwSS67mDNASt9lhHM09iG8ear&link=" + encodeURIComponent(tab.url));
 });
